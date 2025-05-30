d2ef6dab5ddd83d5353e0a437310be3ebc821d5c
THARUUX <tdamsara6@gmail.com>
2025-05-30 11:53:25 +0530

    30/5

--- components/header.php
+++ components/header.php
@@ -1,24 +1,38 @@
-<div class="w-full bg-[#02515A] flex justify-between items-center px-6 py-3">
-    <div class="flex grow gap-10">
-        <div>+94 77 700 7707</div>
-        <div>info@happymangotours.com</div>
+<div class="w-full bg-[#02515A] flex justify-between items-center px-6 py-3 text-xs sm:text-md">
+    <div class="sm:flex grow gap-10 hidden">
+        <div class="flex gap-2">
+            <img src="/Assets/Group 7004.png" alt="">
+            <div class="text-nowrap">
+                +94 77 700 7707
+            </div>
+        </div>
+        <div class="flex gap-2">
+            <img src="/Assets/Group 7002.png" alt="">
+            <div>
+                info@happymangotours.com
+            </div>
+        </div>
     </div>
-    <div class="flex grow gap-10 justify-end items-center">
+    <div class="flex grow gap-7 justify-end items-center">
         <div>Daily Updates</div>
+        <img src="/Assets/Path 107424.png" alt="">
+        <img src="/Assets/Path 107423.png" alt="">
+        <img src="/Assets/Group 22923.png" alt="">
+        <img src="/Assets/Group 22924.png" alt="">
         <div>
             <button class="px-3 py-2 bg-[#FF9933] text-white  text-xs">WHATSAPP</button>
         </div>
     </div>
 </div>
             
-<div class="w-full flex justify-center bg-white text-black font-bold text-sm items-center py-5">
-    <a href="/" class="px-10">HOME</a>
-    <a href="about.php" class="px-10">ABOUT</a>
-    <a href="tour-packages.php" class="px-10">TOUR PACKAGES</a>
-    <a href="/" class="px-42 flex justify-center items-center">
+<div class="w-full flex justify-center bg-white text-black font-bold text-xs sm:text-sm items-center py-5">
+    <a href="/" class="sm:px-10">HOME</a>
+    <a href="about.php" class="sm:px-10">ABOUT</a>
+    <a href="tour-packages.php" class="sm:px-10">TOUR PACKAGES</a>
+    <a href="/" class="sm:px-42 flex justify-center items-center">
         <img class="h-full w-full" src="Assets/logo.png" alt="">
     </a>
-    <a href="gallery.php" class="px-10">GALLERY</a>
-    <a href="blog.php" class="px-10">BLOG</a>
-    <a href="contact.php" class="px-10">CONTACT</a>
+    <a href="gallery.php" class="sm:px-10">GALLERY</a>
+    <a href="blog.php" class="sm:px-10">BLOG</a>
+    <a href="contact.php" class="sm:px-10">CONTACT</a>
 </div>
--- tpd.php
+++ tpd.php
@@ -23,7 +23,178 @@
             <!-- Header -->
             <?php include 'components/header.php'; ?>
 
-            
+            <div class="w-full py-20 flex flex-col justify-center items-center gap-5 bg-[#000000aa]">
+                <div class="text-7xl font-black font-pri">Experience the Best of Sri Lanka</div>
+                <div class="text-2xl font-black font-pri">HOME - TOURS</div>
+            </div>
+
+            <div class="w-full p-10 bg-white">
+                <div class="flex">
+                    <div class="w-1/3 p-20">
+                        <div class="flex flex-col bg-[#FF9933]">
+                            <div class="py-10 border-b flex px-15 gap-10 items-center text-xl font-bold hover:bg-[#02515A] duration-300">
+                                <div>
+                                    <img src="Assets/marker (4).png" alt="">
+                                </div>
+                                <div>ABOUT</div>
+                            </div>
+                            <div class="py-10 border-b flex px-15 gap-10 items-center text-xl font-bold hover:bg-[#02515A] duration-300">
+                                <div>
+                                    <img src="Assets/marker (4).png" alt="">
+                                </div>
+                                <div>INCLUDE AND EXCLUDE</div>
+                            </div>
+                            <div class="py-10 border-b flex px-15 gap-10 items-center text-xl font-bold hover:bg-[#02515A] duration-300">
+                                <div>
+                                    <img src="Assets/marker (4).png" alt="">
+                                </div>
+                                <div>ITINERARY</div>
+                            </div>
+                            <div class="py-10 border-b flex px-15 gap-10 items-center text-xl font-bold hover:bg-[#02515A] duration-300">
+                                <div>
+                                    <img src="Assets/marker (4).png" alt="">
+                                </div>
+                                <div>LOCATION</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="w-2/3 p-20">
+                        <img class="w-full" src="Assets/img(28).png" alt="">
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="w-full bg-white flex text-black p-20 pt-0">
+                <div class="w-2/3 p-10 flex flex-col gap-8">
+                    <div class="w-full text-7xl font-black font-pri">
+                        Experience the Best of Sri Lanka
+                    </div>
+                    <div class="text-3xl"><span class="font-bold">PRICE $119 /</span> 5Days Trip</div>
+                    <span class="w-3/5 h-[1px] bg-black"></span>
+                    <div class="flex w-full gap-10">
+                        <div class="flex text-2xl gap-5">
+                            <img src="Assets/calendar-clock.png" alt="">
+                            <div class="text-nowrap">5Days Trip</div>
+                        </div>
+                        <div class="flex text-2xl gap-5">
+                            <img src="Assets/footprint.png" alt="">
+                            <div class="text-nowrap">Daily Tour</div>
+                        </div>
+                        <div class="flex text-2xl gap-5">
+                            <img src="Assets/team-check-alt.png" alt="">
+                            <div class="text-nowrap">25 people</div>
+                        </div>
+                    </div>
+                    <span class="w-3/5 h-[1px] bg-black"></span>
+                    <div>
+                        Discover the heart and soul of Sri Lanka with our carefully crafted "Best of Sri Lanka Tour." From ancient cities and lush hill country to golden beaches and thrilling safaris, this journey brings together the island’s most iconic destinations, culture, and natural wonders — all in one unforgettable adventure.
+                    </div>
+                    <span class="w-3/5 h-[1px] bg-black"></span>
+                    <div class="text-5xl font-black font-pri">About this tour</div>
+                    <div>
+                        Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of  classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. 
+                    </div>
+                    <span class="w-3/5 h-[1px] bg-black"></span>
+                    <div class="text-5xl font-black font-pri">Included/Excluded</div>
+                    <div class="w-4/5 flex">
+                        <div class="w-1/2 flex-col flex gap-3">
+                            <div>Meet and assistance at the Colombo Airport.</div>
+                            <div>Specialized bilingual guide</div>
+                            <div>All fuel, insurance and license fees of the vehicle.</div>
+                            <div>Replacement of vehicle in case of breakdown.</div>
+                        </div>
+                        <div class="w-1/2 flex-col flex gap-3">
+                            <div>Meet and assistance at the Colombo Airport.</div>
+                            <div>Specialized bilingual guide</div>
+                            <div>All fuel, insurance and license fees of the vehicle.</div>
+                            <div>Replacement of vehicle in case of breakdown.</div>
+                        </div>
+                    </div>
+                    <span class="w-3/5 h-[1px] bg-black"></span>
+                    <div class="text-5xl font-black font-pri">Itinerary</div>
+                    <div class="flex flex-col gap-5">
+                        <div class="flex flex-col bg-[#F6F6F6] rounded-2xl">
+                            <div class="flex gap-10 p-5 border-b border-slate-600 items-center">
+                                <button class="text-white bg-[#02515A] rounded-full py-2 px-4">DAY 01</button>
+                                <div class="text-xl">Kandy</div>
+                            </div>
+                            <div class="w-full p-5 flex gap-5">
+                                <img src="Assets/Image 4.png" class="" alt="">
+                                <div>
+                                    Arrive in Sri Lanka to a warm welcome from our guide at the airport. Explore the Pinnawala Elephant Orphanage on your way to Kandy, known for the world's largest captive elephant herd. Founded in 1975, the sanctuary cares for orphaned elephants.
+                                    <br><br>
+                                    After checking into your hotel, relax and attend the Kandy Cultural Dance Show in the evening. Visit the iconic Tooth Relic Temple, "Dalada Maligawa," for a dose of peaceful Buddhist culture with your guide's insights. Sleep overnight as a proud Kandyan, enjoying a journey blending nature and culture.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="flex flex-col bg-[#F6F6F6] rounded-2xl">
+                            <div class="flex gap-10 p-5 border-b border-slate-600 items-center">
+                                <button class="text-white bg-[#02515A] rounded-full py-2 px-4">DAY 02</button>
+                                <div class="text-xl">Kandy</div>
+                            </div>
+                            <div class="w-full p-5 flex gap-5">
+                                <img src="Assets/Image 4.png" class="" alt="">
+                                <div>
+                                    Arrive in Sri Lanka to a warm welcome from our guide at the airport. Explore the Pinnawala Elephant Orphanage on your way to Kandy, known for the world's largest captive elephant herd. Founded in 1975, the sanctuary cares for orphaned elephants.
+                                    <br><br>
+                                    After checking into your hotel, relax and attend the Kandy Cultural Dance Show in the evening. Visit the iconic Tooth Relic Temple, "Dalada Maligawa," for a dose of peaceful Buddhist culture with your guide's insights. Sleep overnight as a proud Kandyan, enjoying a journey blending nature and culture.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="flex flex-col bg-[#F6F6F6] rounded-2xl">
+                            <div class="flex gap-10 p-5 border-b border-slate-600 items-center">
+                                <button class="text-white bg-[#02515A] rounded-full py-2 px-4">DAY 03</button>
+                                <div class="text-xl">Kandy</div>
+                            </div>
+                            <div class="w-full p-5 flex gap-5">
+                                <img src="Assets/Image 4.png" class="" alt="">
+                                <div>
+                                    Arrive in Sri Lanka to a warm welcome from our guide at the airport. Explore the Pinnawala Elephant Orphanage on your way to Kandy, known for the world's largest captive elephant herd. Founded in 1975, the sanctuary cares for orphaned elephants.
+                                    <br><br>
+                                    After checking into your hotel, relax and attend the Kandy Cultural Dance Show in the evening. Visit the iconic Tooth Relic Temple, "Dalada Maligawa," for a dose of peaceful Buddhist culture with your guide's insights. Sleep overnight as a proud Kandyan, enjoying a journey blending nature and culture.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="flex flex-col bg-[#F6F6F6] rounded-2xl">
+                            <div class="flex gap-10 p-5 border-b border-slate-600 items-center">
+                                <button class="text-white bg-[#02515A] rounded-full py-2 px-4">DAY 04</button>
+                                <div class="text-xl">Kandy</div>
+                            </div>
+                            <div class="w-full p-5 flex gap-5">
+                                <img src="Assets/Image 4.png" class="" alt="">
+                                <div>
+                                    Arrive in Sri Lanka to a warm welcome from our guide at the airport. Explore the Pinnawala Elephant Orphanage on your way to Kandy, known for the world's largest captive elephant herd. Founded in 1975, the sanctuary cares for orphaned elephants.
+                                    <br><br>
+                                    After checking into your hotel, relax and attend the Kandy Cultural Dance Show in the evening. Visit the iconic Tooth Relic Temple, "Dalada Maligawa," for a dose of peaceful Buddhist culture with your guide's insights. Sleep overnight as a proud Kandyan, enjoying a journey blending nature and culture.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <span class="w-3/5 h-[1px] bg-black"></span>
+                    <div class=""><span class="text-5xl font-black font-pri">Tour's Location </span><span class="text-md" >Kandy | Colombo | Matara | galle</span></div>
+                    <div>
+                        <img class="w-full" src="Assets/Screenshot 2025-04-27 134546.png" alt="">
+                    </div>
+                </div>
+                <div class="w-1/3">
+                    <div class="bg-[#02515A] w-full flex flex-col p-20  text-white">
+                        <div class="text-5xl font-pri font-black">Inquairy Now</div>
+                        <div class="py-3">It’s time to plan just the perfect vacation!</div>
+                        <div class="flex flex-col gap-5">
+                            <input type="text" placeholder="NAME" class="bg-[#00000000] border-0 border-b border-slate-400 py-3">
+                            <input type="text" placeholder="EMAIL" class="bg-[#00000000] border-0 border-b border-slate-400 py-3">
+                            <input type="tel" placeholder="PHONE" class="bg-[#00000000] border-0 border-b border-slate-400 py-3">
+                            <input type="date" placeholder="DATE" class="bg-[#00000000] border-0 border-b border-slate-400 py-3">
+                            <input type="number" placeholder="NUMBER OF ADULTS" class="bg-[#00000000] border-0 border-b border-slate-400 py-3">
+                            <textarea type="" placeholder="MESSAGE" class="bg-[#00000000] border-0 border-b border-slate-400 py-3"></textarea>
+                        </div>
+                        <div class="w-full flex justify-center py-20">
+                            <button class="w-full bg-[#FF9933] py-3 rounded-full text-md font-bold">SEND NOW</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
             
             <?php include 'components/comp1.php'; ?>
